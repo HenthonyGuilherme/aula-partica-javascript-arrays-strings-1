59cd42bb71877d51fc0eb6af01ac70344a4d1f3b
HenthonyGuilherme <henthonyguilhermeferreira.2021@gmail.com>
2021-09-27 18:32:29 -0300

    @_@

---
+++ AT1-JS-.js
@@ -0,0 +1,18 @@
+// 1º)
+
+var dominio1 = "mundojs"
+var dominio2 = "google"
+console.log(`Olá ${dominio1}! O site www.${dominio2}.com te ajuda a achar muitos materiais de estudo.`);
+
+// 2º)
+
+var valorA = "casa"
+var valorB = "asa"
+console.log(valorA.toUpperCase());
+console.log(valorB.toUpperCase());
+console.log(valorA.localeCompare(valorB));
+
+// 3º)
+ var valorC = "substring(): Aprenda a utilizar.";
+ var cortei = valorC.substring(13, 31);
+console.log(cortei);
---
+++ AT2-JS-.js
@@ -0,0 +1,32 @@
+// 1º)
+var tabuadaDe5 = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+var stng = tabuadaDe5.toString();
+console.log(stng);
+
+// 2º)
+
+var temperaturas = [54, 78, 56, 98, 0, 12, 11, 37]
+
+var cel1 = (54 - 32) / 1.8;
+console.log('54 °F em Celsius é: ' + cel1);
+
+var cel2 = (78 - 32) / 1.8;
+console.log('78 °F em Celsius é: ' + cel2);
+
+var cel3 = (56 - 32) / 1.8;
+console.log('56 °F em Celsius é: ' + cel3);
+
+var cel4 = (98 - 32) / 1.8;
+console.log('98 °F em Celsius é: ' + cel4);
+
+var cel5 = (0 - 32) / 1.8;
+console.log('0 °F em Celsius é: ' + cel5);
+
+var cel6 = (12 - 32) / 1.8;
+console.log('12 °F em Celsius é: ' + cel6);
+
+var cel7 = (11 - 32) / 1.8;
+console.log('11 °F em Celsius é: ' + cel7);
+
+var cel8 = (37 - 32) / 1.8;
+console.log('37 °F em Celsius é: ' + cel8);
